a9d02126bee8f3964554a6f13f4bb6417f6bc7fa
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2026-01-06 16:47:30 -0700

    Update main.go

--- main.go
+++ main.go
@@ -9,20 +9,22 @@ import (
 func main() {
 
 	var host string
-	
-	// Load and set config variables
+
 	cfg := config.LoadConfig()
-	api.SetAPIKey(cfg.APIKey)
-	api.SetAPIAddress(cfg.Address, cfg.APIPort)
-	web.SetAPIKey(cfg.APIKey)
-	web.SetAPIAddress(cfg.Address, cfg.WebPort)
-	web.SetCredentials(cfg.Username, cfg.Password)
 
 	if cfg.Address == "0.0.0.0" || cfg.Address == "localhost" || cfg.Address == "" {
 		host = "localhost"
 	} else {
 		host = cfg.Address
 	}
+
+	// Load and set config variables
+	api.SetAPIKey(cfg.APIKey)
+	api.SetAPIAddress(host, cfg.APIPort)
+	web.SetAPIKey(cfg.APIKey)
+	web.SetAPIAddress(host, cfg.WebPort)
+	web.SetCredentials(cfg.Username, cfg.Password)
+
 	
 	addrAPI := host + ":" + cfg.APIPort
 	addrWeb := host + ":" + cfg.WebPort
