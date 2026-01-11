6f8ec8030a87417442d8e5794238712706551f5c
tschettervictor <tschetter.victor@gmail.com>
2026-01-10 19:14:59 -0700

    move some files around

--- web/web.go
+++ web/web.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"path/filepath"
+	"os"
 )
 
 func callBastilleAPI(path string, params map[string]string) (string, error) {
@@ -47,24 +49,55 @@ func callBastilleAPI(path string, params map[string]string) (string, error) {
 	return string(body), nil
 }
 
-// --- Template renderer ---
 func render(w http.ResponseWriter, page string, data PageData) {
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tmpl, err := template.ParseFiles(
-		"web/static/partials/default.html",
-		"web/static/partials/sidebar.html",
-		"web/static/partials/navbar.html",
-		"web/static/partials/options.html",
-		"web/static/home.html",
-		"web/static/login.html",
-		"web/static/settings.html",
-		"web/static/nodes.html",
-		"web/static/"+page+".html",
-	)
+
+	// Get all subcommand files
+	templatePages, err := filepath.Glob("web/static/templates/*.html")
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
+
+	partialsPages, err := filepath.Glob("web/static/partials/*.html")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	mainPages := []string{
+        "web/static/home.html",
+        "web/static/login.html",
+        "web/static/settings.html",
+        "web/static/nodes.html",
+		"web/static/bastille-logo.png",
+	}
+
+    requestedPage := page
+    if !filepath.IsAbs(requestedPage) {
+        requestedPage = filepath.Join("web/static", requestedPage+".html")
+    }
+
+    // Ensure it exists
+    if _, err := os.Stat(requestedPage); os.IsNotExist(err) {
+        http.Error(w, "Page not found: "+requestedPage, 404)
+        return
+    }
+
+	// Combine all files
+	files := []string{} 
+	files = append(files, templatePages...)
+	files = append(files, partialsPages...)
+	files = append(files, mainPages...)
+	files = append(files, requestedPage)
+
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
 	err = tmpl.ExecuteTemplate(w, "default", data)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
